788c2c8155cee1cb1f29374815f335f6244d13b4
nii_okantey <nunoogideon19@gmail.com>
2023-10-31 12:52:39 +0000

    updates

--- src/components/CustomTopTab.jsx
+++ src/components/CustomTopTab.jsx
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
-import BackButton from "./BackButton";
+import { FontAwesome5 } from "@expo/vector-icons";
 export default CustomTopTab = ({ name }) => {
   return (
     <View className="flex px-4 flex-row items-center ">
-      <BackButton onPress={() => navigation.goBack()} />
-      <Text style={{ fontSize: 24, fontFamily: "lato-bold" }}>{name}</Text>
+      <FontAwesome5 name="user-graduate" size={36} color="black" />
+      <Text style={{ fontSize: 24, fontFamily: "lato-bold" }} className="ml-4">
+        {name}
+      </Text>
     </View>
   );
 };
